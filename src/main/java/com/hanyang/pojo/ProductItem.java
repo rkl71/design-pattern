0ded097da0bc85af1fe164b49079838a6260b226
rkl71 <renkelin00@gmail.com>
2024-03-11 21:35:25 +0800

    feature: 初始化商品类目信息

---
+++ src/main/java/com/hanyang/pojo/ProductItem.java
@@ -0,0 +1,20 @@
+package com.hanyang.pojo;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "product_item")
+public class ProductItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int pid;
+}
