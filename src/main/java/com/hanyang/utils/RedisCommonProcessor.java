97b0f1448244c1883ca46a110c51bea3674a24ba
rkl71 <renkelin00@gmail.com>
2024-03-11 21:35:01 +0800

    feature: 集成 Redis

---
+++ src/main/java/com/hanyang/utils/RedisCommonProcessor.java
@@ -0,0 +1,32 @@
+package com.hanyang.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisCommonProcessor {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public Object get(String key) {
+        if (key == null) {
+            throw new UnsupportedOperationException("Redis key could not be null!");
+        }
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public void set(String key, Object value, long time) {
+        if (time > 0) {
+            redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+        } else {
+            set(key, value);
+        }
+    }
+}
